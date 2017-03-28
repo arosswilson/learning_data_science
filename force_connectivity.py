31f7400ee894b247591f8fa1327f2d02a2eb9777
rosswilson <arthur.ross.wilson@gmail.com>
2017-03-28 08:35:23 -0400

    create objects for force data

---
+++ force_connectivity.py
@@ -0,0 +1,111 @@
+import sys, csv, os
+
+
+class Record(object):
+    """Represents a row"""
+
+class Case(Record):
+    """Represents a case from Force"""
+
+class Table(object):
+    """list of objects"""
+
+    def __init__(self):
+        self.records = []
+
+    def __len__(self):
+        return len(self.records)
+
+    def ReadFile(self, data_dir, filename, constructor, n=None):
+        """ Reads a file and makes a object for each row
+
+        Args:
+            data_dir: string directory name
+            filename: string name of the file
+            fields: sequence of (name, start, end, case) tuples specifying the fields to extract
+
+            constructor: what kind of object to create
+        """
+
+        filename = os.path.join(data_dir, filename)
+        with open(filename, 'r') as f:
+            sheet = csv.DictReader(f)
+
+            for i, row in enumerate(sheet):
+                if i == n:
+                    break
+
+                record = self.MakeRecord(row, constructor)
+                self.AddRecord(record)
+
+    def MakeRecord(self, row, constructor):
+        """ Takes the csv row and returns and object with fields
+
+        Args:
+            line: line number of row
+            fields: sequence of (name, start, end, case) tuples specifying the fields to extract
+            constructor: callable that makes an object for the record
+
+        return:
+            record with fields defined by columns in csv
+        """
+
+        obj = constructor()
+        for title, value in row.items():
+            try:
+                val = float(val)
+            except:
+                val = value
+
+            setattr(obj, title, val)
+
+        return obj
+
+    def AddRecord(self, record):
+        """adds a record to its Table
+
+        Args:
+            record: and object of one of the record types
+        """
+        self.records.append(record)
+
+    def ExtendRecords(self, records):
+        """ Adds records to this Table
+
+        Args:
+            records: an iterable of record obj
+
+        """
+        self.recrods.extend(records)
+
+    def Recode(self):
+        """ Child classes can override this to recode values"""
+        pass
+
+class Cases(Table):
+    """Represents the cases Table. """
+
+    def ReadRecords(self, data_dir='.', n=None):
+        filename = self.GetFilename()
+        self.ReadFile(data_dir, filename, Case, n)
+        self.Recode()
+
+    def GetFilename(self):
+        return 'force_connectivity_report.csv'
+
+def main(name, data_dir='.'):
+    cases = Cases()
+    cases.ReadRecords(data_dir)
+
+    print('Number of cases %s'% len(cases.records))
+
+if __name__ == '__main__':
+    main(*sys.argv)
+
+
+
+
+
+
+
+
